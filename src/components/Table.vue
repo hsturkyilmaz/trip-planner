8bfc04522306df229a0d90da541de7087c12d098
Hasan Sezai Türkyılmaz <hasan.turkyilmaz@akinon.com>
2021-09-28 01:01:06 +0300

    Create vue project

---
+++ src/components/Table.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="table">
+    TABLE
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Table',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import * as VueGoogleMaps from "vue2-google-maps" // Import package
+
 Vue.config.productionTip = false
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: "AIzaSyAYl529Wtd8vQVZh0BKYBSJsdbvVHDHgGI",
+    libraries: "places"
+  }
+});
 
 new Vue({
   render: h => h(App),
